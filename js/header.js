3ed63f28f8bb5e675cd87aecfd53c5ff9db3920e
Cwmfest <113729815+Cwmfest@users.noreply.github.com>
2023-09-15 03:22:28 +0100

    Synchronised changes from the main repository
    Updated the files to match the main repo to make sure everything matches before I test out anything new

--- js/header.js
+++ js/header.js
@@ -9,6 +9,7 @@ const headerContent = `<meta http-equiv="X-Clacks-Overhead" content="GNU Terry P
 				<li><a href="lineup.html">Lineup</a></li>
 				<!-- <li><a href="programme.html">Programme</a></li> -->
 				<li><a href="map.html">Map</a></li>
+				<li><a href="about.html">About Cwmfest</a></li>
 			</ul>
 		</nav>
 		<br>
